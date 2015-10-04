8f60b678328a96bf920c533a5d6ebc8528ca7c67
Ryan P.C. McQuen <ryan.q@linux.com>
2015-10-04 10:09:31 -0700

    remove needless var
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- scrollEvents.js
+++ scrollEvents.js
@@ -1,10 +1,9 @@
-// version 0.2.0
+// version 0.2.1
 var scrollEvents = {
   change: function (selectors, breakPoint, property, initialValue, changedValue) {
     window.onscroll = function (event) {
       // grab the selectors and convert them into an array so we can use forEach()
-      var items = document.querySelectorAll(selectors);
-      var itemArray = [].slice.call(items);
+      var itemArray = [].slice.call(document.querySelectorAll(selectors));
       // window.pageYOffset has better compatibility than document.body.scrollTop
       var scrollPos = window.pageYOffset;
       if (scrollPos > breakPoint) {

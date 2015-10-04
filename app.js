f5385a4ea1eeab8718bd449c26c6e5a72501bed3
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-10-04 15:33:56 -0700

    spice up the demo a bit

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 scrollEvents.changeStyle(".foo", "height", "400px", "800px");
+scrollEvents.changeStyle(".foo", "backgroundColor", "#008cff", "#00ad85");
 scrollEvents.changeText("#corner__box", "At the top.", "You have scrolled!");

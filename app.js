9033ebb6e9ef74fe957a432126d13283d994af89
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-10-04 15:37:38 -0700

    changeText on box

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 scrollEvents.changeStyle(".foo", "height", "400px", "800px");
 scrollEvents.changeStyle(".foo", "backgroundColor", "#008cff", "#00ad85");
+scrollEvents.changeText(".foo", "Scroll to increase the box height.", "Now we are tall and green!");
 scrollEvents.changeText("#corner__box", "At the top.", "You have scrolled!");

0e69f13772ee4761b28114639966685157eeb303
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-10-09 08:48:15 -0700

    rewording

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ scrollEvents.changeStyle(".foo", "backgroundColor", "#008cff", "#00ad85");
 scrollEvents.changeText(".foo", "Scroll down to change stuff.", "Now we are taller and greener!");
 scrollEvents.changeText("#corner__box", "At the top.", "You have scrolled!");
 scrollEvents.changeClass('.bar', 'nonscrolled', 'scrolled');
-scrollEvents.changeHTML('.bar', 'Scroll down to change css class.', 'We use changeClass. <br> <= That guy uses inline styles, EWWW.');
+scrollEvents.changeHTML('.bar', 'Scroll down to change class.', 'We changed class. <br> <= That guy uses inline styles, EWWW.');
 
 // need to make this happen
 //scrollEvents.changeClass('.bar', 'nonscrolled', 'scrolled').changeText('.bar', 'Initial.', 'Changed.');

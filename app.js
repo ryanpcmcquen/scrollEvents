5bb9ea6f5ec0f04a70f8493e9094a8a75fd9cc20
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-10-09 11:28:05 -0700

    show off method chaining

--- app.js
+++ app.js
@@ -2,8 +2,5 @@ scrollEvents.changeStyle(".foo", "height", "25%", "75%");
 scrollEvents.changeStyle(".foo", "backgroundColor", "#008cff", "#00ad85");
 scrollEvents.changeText(".foo", "Scroll down to change stuff.", "Now we are taller and greener!");
 scrollEvents.changeText("#corner__box", "At the top.", "You have scrolled!");
-scrollEvents.changeClass('.bar', 'nonscrolled', 'scrolled');
-scrollEvents.changeHTML('.bar', 'Scroll down to change class.', 'We changed class. <br> <= That guy uses inline styles, EWWW.');
-
-// need to make this happen
-//scrollEvents.changeClass('.bar', 'nonscrolled', 'scrolled').changeText('.bar', 'Initial.', 'Changed.');
+// the sexy new method chaining
+scrollEvents('bar').changeClass('nonscrolled', 'scrolled').changeHTML('Scroll down to change class.', 'We changed class. <br> <= That guy uses inline styles, EWWW.');

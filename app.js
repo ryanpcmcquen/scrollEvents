3a7d39a31a42df6ce211e3ff2fca982b34d59d2b
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-10-09 17:33:08 -0700

    fix .bar selector

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ scrollEvents.changeStyle(".foo", "backgroundColor", "#008cff", "#00ad85");
 scrollEvents.changeText(".foo", "Scroll down to change stuff.", "Now we are taller and greener!");
 scrollEvents.changeText("#corner__box", "At the top.", "You have scrolled!");
 // the sexy new method chaining
-scrollEvents('bar').changeClass('nonscrolled', 'scrolled').changeHTML('Scroll down to change class.', 'We changed class. <br> <= That guy uses inline styles, EWWW.');
+scrollEvents('.bar').changeClass('nonscrolled', 'scrolled').changeHTML('Scroll down to change class.', 'We changed class. <br> <= That guy uses inline styles, EWWW.');

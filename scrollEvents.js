4a9a79cfd68ee09615b99a317c2efededeee71a4
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-02 06:11:28 -0700

    P. C.

--- scrollEvents.js
+++ scrollEvents.js
@@ -1,7 +1,7 @@
 // @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2-or-later
 /*! scrollEvents v1.1.4 by ryanpcmcquen */
 //
-// Ryan P.C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
+// Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by

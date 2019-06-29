8c3dd93aa298002ac257500ce4321bb10b4de71b
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-06-29 11:19:49 -0700

    Switch to MPLv2.

--- scrollEvents.js
+++ scrollEvents.js
@@ -1,4 +1,4 @@
-// @license magnet:?xt=urn:btih:cf05388f2679ee054f2beb29a391d25f4e673ac3&dn=gpl-2.0.txt GPL-v2-or-later
+// @license magnet:?xt=urn:btih:3877d6d54b3accd4bc32f8a48bf32ebc0901502a&dn=mpl-2.0.txt MPL-v2
 /*! scrollEvents v1.1.4 by ryanpcmcquen */
 //
 // Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org

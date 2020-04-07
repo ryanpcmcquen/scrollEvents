db769fffbe095e1637d8a3ed682e4377c83088e9
Ryan P. C. McQuen <rpcm@linux.com>
2020-04-07 13:30:17 -0700

    Remove email.

--- scrollEvents.js
+++ scrollEvents.js
@@ -1,7 +1,7 @@
 // @license magnet:?xt=urn:btih:3877d6d54b3accd4bc32f8a48bf32ebc0901502a&dn=mpl-2.0.txt MPL-v2
 /*! scrollEvents v1.1.4 by ryanpcmcquen */
 //
-// Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
+// Ryan P. C. McQuen | Everett, WA
 
 /*global window*/
 /*jslint browser: true, white:true*/
